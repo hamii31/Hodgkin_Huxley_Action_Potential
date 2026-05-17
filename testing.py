7bd73d8c5815f1a41fefa1fa5010eb2355e22487
Hami Ibriyamov <hamop3108@gmail.com>
2026-05-17 13:01:23 +0200

    Refactor testing structure and improve clarity
    Refactor tests for rate constants, steady state values, gating ODEs, ionic currents, and dynamical system. Consolidate print statements and improve structure for clarity.

--- testing.py
+++ testing.py
@@ -1,10 +1,16 @@
 import math
+from rate_constants import alpha_m, alpha_h, alpha_n, beta_m, beta_h, beta_n
+from steady_state_values import m_inf, h_inf, n_inf, tau_m, tau_h, tau_n
+from gating_ODEs import m_gating, h_gating, n_gating
+from ionic_currents import sodium_channel, potassium_channel, leaky_channel
+from dynamical_system import dynamical_system
 
 # CONSTANTS
 V_rest = -65
 
-# Test the rate constants
-from rate_constants import alpha_m, alpha_h, alpha_n, beta_m, beta_h, beta_n
+# === Test the rate constants ===
+
+print("Testing rate constants at resting voltage...")
 
 alpha_m_rest = 0.2236
 beta_m_rest = 4.0
@@ -12,16 +18,29 @@ alpha_h_rest = 0.07
 beta_h_rest = 0.04743
 alpha_n_rest = 0.058198
 beta_n_rest = 0.125
-print("Testing rate constants at resting voltage...")
-print(math.isclose(a=alpha_m_rest, b=round(alpha_m(V=V_rest),4)))
-print(math.isclose(a=beta_m_rest, b=beta_m(V=V_rest)))
-print(math.isclose(a=alpha_h_rest, b=alpha_h(V=V_rest)))
-print(math.isclose(a=beta_h_rest, b=round(beta_h(V=V_rest),5)))
-print(math.isclose(a=alpha_n_rest, b=round(alpha_n(V=V_rest),6)))
-print(math.isclose(a=beta_n_rest, b=round(beta_n(V=V_rest),3)))
 
-# Test the steady state values and time constants
-from steady_state import m_inf, h_inf, n_inf, tau_m, tau_h, tau_n
+rate_dict = {"alpha_m_rest":math.isclose(a=alpha_m_rest, b=alpha_m(V=V_rest), abs_tol=1e-3),
+             "beta_m_rest":math.isclose(a=beta_m_rest, b=beta_m(V=V_rest), abs_tol=1e-3),
+             "alpha_h_rest":math.isclose(a=alpha_h_rest, b=alpha_h(V=V_rest), abs_tol=1e-3),
+             "beta_h_rest":math.isclose(a=beta_h_rest, b=beta_h(V=V_rest), abs_tol=1e-3),
+             "alpha_n_rest":math.isclose(a=alpha_n_rest, b=alpha_n(V=V_rest), abs_tol=1e-3),
+             "beta_n_rest":math.isclose(a=beta_n_rest, b=beta_n(V=V_rest), abs_tol=1e-3)}
+
+if all(rate_dict.values()):
+    print("All tests successfully passed.")
+else:
+    find_what_failed = {key: value for key,
+                 value in rate_dict.items() if value == False}
+    
+    print(find_what_failed)
+
+
+
+# === Test the steady state values and time constants ===
+
+ssv_tc_dict = {}
+
+print("Testing steady state values and time constants at resting voltage...")
 
 m_inf_rest = 0.0529
 h_inf_rest = 0.596
@@ -29,28 +48,49 @@ n_inf_rest = 0.318
 tau_m_rest = 0.237
 tau_h_rest = 8.52
 tau_n_rest = 5.46
-print("Testing steady state values and time constants at resting voltage...")
-print(math.isclose(a=m_inf_rest, b=round(m_inf(V=V_rest),4)))
-print(math.isclose(a=h_inf_rest, b=round(h_inf(V=V_rest),3)))
-print(math.isclose(a=n_inf_rest, b=round(n_inf(V=V_rest),3)))
-print(math.isclose(a=tau_m_rest, b=round(tau_m(V_rest),3)))
-print(math.isclose(a=tau_h_rest, b=round(tau_h(V_rest),2)))
-print(math.isclose(a=tau_n_rest, b=round(tau_n(V_rest),2)))
 
-# Test the gating ODEs
-from gating_ODEs import m_gating, h_gating, n_gating
+ssv_tc_dict = {"m_inf_rest":math.isclose(a=m_inf_rest, b=m_inf(V=V_rest), abs_tol=1e-3),
+               "h_inf_rest":math.isclose(a=h_inf_rest, b=h_inf(V=V_rest), abs_tol=1e-3),
+               "n_inf_rest":math.isclose(a=n_inf_rest, b=n_inf(V=V_rest), abs_tol=1e-3),
+               "tau_m_rest":math.isclose(a=tau_m_rest, b=tau_m(V_rest), abs_tol=1e-3),
+               "tau_h_rest":math.isclose(a=tau_h_rest, b=tau_h(V_rest), abs_tol=5e-3),
+               "tau_n_rest":math.isclose(a=tau_n_rest, b=tau_n(V_rest), abs_tol=5e-3)}
+
+if all(ssv_tc_dict.values()):
+    print("All tests successfully passed.")
+else:
+    find_what_failed = {key: value for key,
+                 value in ssv_tc_dict.items() if value == False}
+    
+    print(find_what_failed)
+
+
+
+# === Test the gating ODEs === 
+
+gating_ODEs_dict = {}
 
 print("testing the gating ODEs at resting steady state and resting membrane voltage...")
 
 m_gating_rest = 0.0
 h_gating_rest = 0.0
 n_gating_rest = 0.0
-print(math.isclose(a=m_gating_rest, b=m_gating(V=V_rest, m=m_inf(V=V_rest))))
-print(math.isclose(a=h_gating_rest, b=h_gating(V=V_rest, h=h_inf(V=V_rest))))
-print(math.isclose(a=n_gating_rest, b=n_gating(V=V_rest, n=n_inf(V=V_rest))))
 
-# Test the ionic channels
-from ionic_currents import sodium_channel, potassium_channel, leaky_channel
+gating_ODEs_dict = {"m_gating_rest":math.isclose(a=m_gating_rest, b=m_gating(V=V_rest, m=m_inf(V=V_rest)), abs_tol=1e-3),
+                    "h_gating_rest":math.isclose(a=h_gating_rest, b=h_gating(V=V_rest, h=h_inf(V=V_rest)), abs_tol=1e-3),
+                    "n_gating_rest":math.isclose(a=n_gating_rest, b=n_gating(V=V_rest, n=n_inf(V=V_rest)), abs_tol=1e-3)}
+
+if all(gating_ODEs_dict.values()):
+    print("All tests successfully passed.")
+else:
+    find_what_failed = {key: value for key,
+                 value in gating_ODEs_dict.items() if value == False}
+    
+    print(find_what_failed)
+
+
+
+# === Test the ionic channels ===
 
 print("Testing ionic currents at resting steady state and resting membrane voltage...")
 
@@ -60,10 +100,16 @@ I_Na = sodium_channel(V=V_rest, m=m_inf(V=V_rest), h=h_inf(V=V_rest))
 I_K = potassium_channel(V=V_rest, n=n_inf(V=V_rest))
 I_L = leaky_channel(V=V_rest)
 
-print(math.isclose(a=total_membrane_current_rest, b=round(I_Na + I_K + I_L, 1)))
+if math.isclose(a=total_membrane_current_rest, b=I_Na + I_K + I_L, abs_tol=1e-3):
+    print("All tests successfully passed.")
+else:
+    print("Something isn't right with your ionic currents implementation.")
 
-# Test the dynamical system
-from dynamical_system import dynamical_system
+
+
+# === Test the dynamical system ===
+
+dyn_sys_dict = {}
 
 print("Testing dynamical system at rest...")
 
@@ -75,7 +121,16 @@ dV_dt_rest = 0.0
 dm_dt_rest = 0.0
 dh_dt_rest = 0.0
 dn_dt_rest = 0.0
-print(math.isclose(a=dV_dt_rest, b=round(derivatives[0],1)))
-print(math.isclose(a=dm_dt_rest, b=round(derivatives[1],1)))
-print(math.isclose(a=dh_dt_rest, b=round(derivatives[2],1)))
-print(math.isclose(a=dn_dt_rest, b=round(derivatives[3],1)))
+
+dyn_sys_dict = {"dV_dt_rest":math.isclose(a=dV_dt_rest, b=derivatives[0], abs_tol=1e-3),
+                "dm_dt_rest":math.isclose(a=dm_dt_rest, b=derivatives[1], abs_tol=1e-3),
+                "dh_dt_rest":math.isclose(a=dh_dt_rest, b=derivatives[2], abs_tol=1e-3),
+                "dn_dt_rest":math.isclose(a=dn_dt_rest, b=derivatives[3], abs_tol=1e-3)}
+
+if all(dyn_sys_dict.values()):
+    print("All tests successfully passed.")
+else:
+    find_what_failed = {key: value for key,
+                 value in dyn_sys_dict.items() if value == False}
+    
+    print(find_what_failed)
