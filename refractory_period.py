cac2aa71542a16ef637fce96a0e3b19370b04e07
Hami Ibriyamov <hamop3108@gmail.com>
2026-05-18 17:18:50 +0200

    Add refractory period simulation and plotting

---
+++ refractory_period.py
@@ -0,0 +1,14 @@
+from steady_state import m_inf, h_inf, n_inf
+from forward_euler import forward_euler
+from plot import refractored_action_potential_plot
+
+# Refractory Experiment
+T_end = 50
+dt = 0.01
+V_rest = -65
+initial_state=(V_rest, m_inf(V_rest), h_inf(V_rest), n_inf(V_rest))
+
+for gap in range(1,20):
+    V_history, t_history, _, h_history, n_history = forward_euler(T_end=T_end, dt=dt, initial_state=initial_state, I_ext_function=lambda t, g=gap: 10 if (5 < t < 6) or (5 + g < t < 6 + g) else 0)
+    if max(V_history) >= 0:
+        refractored_action_potential_plot(title=f"Refractory Experiment at {10} µA", subtitle=f"At +{gap}ms gap", V_history=V_history, t_history=t_history, h_history=h_history, n_history=n_history)
