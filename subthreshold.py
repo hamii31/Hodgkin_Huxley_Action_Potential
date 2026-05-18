dfd7c479d0ee62c7bcabe477c27efa190bda975f
Hami Ibriyamov <hamop3108@gmail.com>
2026-05-18 17:17:42 +0200

    Update forward_euler calls to unpack additional values

--- subthreshold.py
+++ subthreshold.py
@@ -12,11 +12,11 @@ V_rest = -65
 initial_state=(V_rest, m_inf(V_rest), h_inf(V_rest), n_inf(V_rest))
 
 for uA in np.arange(6.95, 6.99, 0.001):
-    V_history, t_history = forward_euler(T_end=T_end, dt=dt, initial_state=initial_state, I_ext_function=lambda t, amp=uA: amp if 5 < t < 6 else 0)
+    V_history, t_history, _, _, _ = forward_euler(T_end=T_end, dt=dt, initial_state=initial_state, I_ext_function=lambda t, amp=uA: amp if 5 < t < 6 else 0)
     if max(V_history) < 0:
         threshold = uA
     if max(V_history) >= 0:
         break;
 
-V_history, t_history = forward_euler(T_end=T_end, dt=dt, initial_state=initial_state, I_ext_function=lambda t: threshold if 5 < t < 6 else 0)
+V_history, t_history, _, _, _ = forward_euler(T_end=T_end, dt=dt, initial_state=initial_state, I_ext_function=lambda t: threshold if 5 < t < 6 else 0)
 action_potential_plot(title="Threshold Experiment", subtitle=f"The last subthreshold was observed at {threshold} µA.", V_history=V_history, t_history=t_history)
