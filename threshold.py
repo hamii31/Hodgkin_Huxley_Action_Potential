aea2b4b75a4e82986701017813ba310eb64c8673
Hami Ibriyamov <hamop3108@gmail.com>
2026-05-19 10:48:33 +0200

    Refactor threshold.py to use updated plot function

--- threshold.py
+++ threshold.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from steady_state import m_inf, h_inf, n_inf
 from forward_euler import forward_euler
-from plot import action_potential_plot
+from plot import plot
 
 # Threshold Experiment
 threshold = 0
@@ -13,11 +13,11 @@ initial_state=(V_rest, m_inf(V_rest), h_inf(V_rest), n_inf(V_rest))
 
 # Iterate through a range of microamps ranging from 0-10 to find the threshold of a 1ms stimulation
 for uA in np.arange(0.0, 10.0, 0.01):
-    V_history, t_history, _, _, _ = forward_euler(T_end=T_end, dt=dt, initial_state=initial_state, I_ext_function=lambda t, amp=uA: amp if 5 < t < 6 else 0)
+    V_history, t_history, m_history, h_history, n_history = forward_euler(T_end=T_end, dt=dt, initial_state=initial_state, I_ext_function=lambda t, amp=uA: amp if 5 < t < 6 else 0)
     if max(V_history) >= 0:
         print(f"V crossed 0 mV at {uA} µA")
         threshold = uA
         break
     
 # Plot
-action_potential_plot(title="Threshold Experiment", subtitle=f"The Action Potential was initialized at {threshold} µA.", V_history=V_history, t_history=t_history)
+plot(title="Threshold Experiment", subtitle=f"The Action Potential was initialized at {threshold} µA.", V_history=V_history, t_history=t_history, m_history=m_history, h_history=h_history, n_history=n_history)
