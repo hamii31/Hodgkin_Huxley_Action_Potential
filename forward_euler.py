150911c13653c49c93bdbf7bc89a3aebb6a54f7f
Hami Ibriyamov <hamop3108@gmail.com>
2026-05-19 10:48:04 +0200

    Refactor forward_euler to return additional state variables

--- forward_euler.py
+++ forward_euler.py
@@ -1,6 +1,6 @@
 from dynamical_system import dynamical_system
 from steady_state import m_inf, h_inf, n_inf
-from plot import action_potential_plot
+from plot import plot
 
 def forward_euler(T_end, dt, initial_state, I_ext_function):
     """
@@ -48,7 +48,7 @@ T_end = 50
 dt = 0.01
 V_rest = -65
 initial_state=(V_rest, m_inf(V_rest), h_inf(V_rest), n_inf(V_rest))
-V_history, t_history, _, _, _ = forward_euler(T_end=T_end, dt=dt, initial_state=initial_state, I_ext_function=lambda t: 10 if 4 < t < 7 else 0)
+V_history, t_history, m_history, h_history, n_history = forward_euler(T_end=T_end, dt=dt, initial_state=initial_state, I_ext_function=lambda t: 10 if 4 < t < 7 else 0)
 
 # Plot
-action_potential_plot(title="Action potential initialization", subtitle=None, V_history=V_history, t_history=t_history)
+plot(title="Action potential initialization", subtitle=None, V_history=V_history, t_history=t_history, m_history=m_history, h_history=h_history, n_history=n_history)
