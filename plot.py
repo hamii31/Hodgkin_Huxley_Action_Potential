91c047d1b2ecf136bd04a274786ffed9550d3d70
Hami Ibriyamov <hamop3108@gmail.com>
2026-05-19 10:47:35 +0200

    Improved plot.py

--- plot.py
+++ plot.py
@@ -1,23 +1,6 @@
 import matplotlib.pyplot as plt
 
-def action_potential_plot(title, subtitle, V_history, t_history):
-    if subtitle == None:
-        plt.suptitle(title)
-        plt.plot(t_history, V_history)
-        plt.xlabel('time (ms)')
-        plt.ylabel('V (mV)')
-        plt.ylim(-80, 60)
-        plt.show()
-    else:
-        plt.suptitle(title)
-        plt.title(subtitle)
-        plt.plot(t_history, V_history)
-        plt.xlabel('time (ms)')
-        plt.ylabel('V (mV)')
-        plt.ylim(-80, 60)
-        plt.show()
-
-def refractored_action_potential_plot(title, subtitle, V_history, h_history, n_history, t_history):
+def plot(title, subtitle, V_history, t_history, m_history, h_history, n_history):
     if subtitle == None:
         fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
         ax1.plot(t_history, V_history, color='blue')
