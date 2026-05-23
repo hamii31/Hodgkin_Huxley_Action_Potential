52a1b221563e476f3f855fa1cd0243e89949abd7
Hami Ibriyamov <hamop3108@gmail.com>
2026-05-23 09:11:25 +0200

    Update import path for steady state values

--- single-neuron-experiments/subthreshold.py
+++ single-neuron-experiments/subthreshold.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from steady_state_values import m_inf, h_inf, n_inf
+from steady_state import m_inf, h_inf, n_inf
 from forward_euler import forward_euler
 from plot import plot
 
